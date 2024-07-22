5f39240ba250e77fb50a9545e1381cc1d3839191
Zane Yu <zongzeyu183@gmail.com>
2024-07-22 20:15:04 +0930

    solve tailwindcss bugs

--- Inventor/src/App.jsx
+++ Inventor/src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, NavLink, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, NavLink } from "react-router-dom";
 import FruitsList from './FruitsList'
 import SpicesList from './SpicesList'
 import VegetablesList from './VegetablesList'
 
 function App() {
   const getActiveClass = ({ isActive }) => 
-    isActive ? 'p-2 bg-green-500 rounded' : 'p-2 bg-gray-500 rounded';
+    isActive ? 'p-2 bg-green-400 rounded' : 'p-2 bg-gray-200 rounded';
 
   return (
     <Router>
@@ -15,14 +15,16 @@ function App() {
           <h1 className="text-3xl font-bold">Inventory</h1>
         </header>
         
-        <nav className="flex space-x-4 my-4">
-          <NavLink to="/vegetables" className={getActiveClass}>(1) Vegetables</NavLink>
-          <NavLink to="/spices" className={getActiveClass}>(2) Spices</NavLink>
-          <NavLink to="/fruits" className={getActiveClass}>(3) Fruits</NavLink>
-        </nav>
+        <div className="space-x-2 bg-gray-50 border-gray-100">
+          <nav className="flex space-x-4 my-4">
+            <NavLink to="/vegetables" className={getActiveClass}>(1) Vegetables</NavLink>
+            <NavLink to="/spices" className={getActiveClass}>(2) Spices</NavLink>
+            <NavLink to="/fruits" className={getActiveClass}>(3) Fruits</NavLink>
+          </nav>
+        </div>
+        
         <main className="w-full flex justify-center">
           <Routes>
-            <Route path="/" element={<Navigate to="/vegetables" />} />
             <Route path="/vegetables" element={<VegetablesList />} />
             <Route path="/spices" element={<SpicesList />} />
             <Route path="/fruits" element={<FruitsList />} />
